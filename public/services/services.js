076b915883d03e3e8aa56bc41d5cf28a1f72b10d
Ashish Mathur <reachashishmathur@gmail.com>
2017-03-05 20:16:11 +0530

    Updated the services description page. Stiull Work in Progress.

--- public/services/services.js
+++ public/services/services.js
@@ -1,50 +1,121 @@
 
-function openCategory(event, catName) {
+function createNode(tag, parent, attrs, innerText) {
+  var child = document.createElement(tag);
+  parent.appendChild(child);
+
+  // Create attributes and add them to the tag
+  for (i = 0; i < attrs.length; i++) {
+    createAttribute(attrs[i], child);
+  }
+
+  child.innerText = innerText;
+  return child;
+}
+
+function createAttribute(attrdesc, node) {
+  var attr = document.createAttribute(attrdesc.name);
+  attr.value = attrdesc.value;
+  node.setAttributeNode(attr);
+}
+
+function loaddata() {
+  
+  var i, j, category, service, catblock, tabblock;
+
+  tabblock = document.getElementById("toc");
+
+  catblock = document.getElementById("category");
+
+  for (i = 0; i < data.length; i++) {
+    var category = data[i];
+    var catName = category.category;
+
+    // create the link for this category
+    var a = createNode("a", catblock, [
+      { "name":"index",   "value":i },
+      { "name":"href",    "value": "javascript:void(0)"},
+      { "name":"name",    "value":catName },
+      { "name":"class",   "value":"catlink" }, 
+      { "name":"onclick", "value":"openCategory(event, \'" + i + "\')" } ], 
+      catName);
+
+    // Create the services div
+    var services = data[i].services;
+    var servicesblock = document.getElementById("servicesblock");
+
+    // create the services block for this category
+    // eg. <div id="customer_services" class="customer services">
+    var serviceblock = createNode("div", servicesblock, [
+      { "name":"id",      "value":data[i].category + "_services" },
+      { "name":"class",   "value":data[i].category + " services" } ],
+      "");
+
+    for (j = 0; j < services.length; j++) {
+      // eg <a href="javascript:void(0)" name="customer_service_create" class="customer service" onclick="openService(event, 'customer', 'create')">Create</a>
+      var service = services[j];
+      var n = createNode("a", serviceblock, [
+        { "name":"href",      "value":"javascript:void(0)" },
+        { "name":"name",      "value":catName + "_service_" + service.name},
+        { "name":"catindex",  "value":i },
+        { "name":"serviceindex", "value":j },
+        { "name":"class",     "value":catName + " service"},
+        { "name":"onclick",   "value":"openService(event, " + i + ", " + j + ")" } ], //, \'" + data[i].category + "\', \'" + data[i].services[j].name + "\')"} ],
+        service.name);
+      }
+    }
+  }
+
+  
+function openCategory(event, catIndex) {
+  var catData = data[catIndex];
 
 	// Categories are always Visible
-    var i, j, list, services_section, category, service, selected;
+    var i, j, list, services_section, category, service, selected, services;
     
- 	// UNSELECT
- 	// Unselect all previously selected categories
- 	selected = document.querySelectorAll("div.category a.selected");
- 	for (i = 0; i < selected.length; i++) {
- 		selected[i].className = selected[i].className.replace(" selected", "");
+ 	// About prior selected category and services ...
+  selCat = document.querySelectorAll("div.category a.selected");
+ 	for (i = 0; i < selCat.length; i++) {
+    // Unselect the category
+ 		selCat[i].className = selCat[i].className.replace(" selected", "");
+    selCatIndex = parseInt(selCat[i].getAttribute("index"));
+    category = data[selCatIndex];
+
+      // UNSELECT and hide all services from that category that was previously selected
+    services = document.querySelectorAll("div." + category.category + ".services");
+    for (i = 0; i < services.length ; i++) {
+      services[i].className = services[i].className.replace(" selected", "");
+      services[i].style.display="none";
+    }
+
  	}
- 	
- 	selected = document.querySelectorAll("div.services");
-    for (i = 0; i < selected.length ; i++) {
-    	selected[i].className = selected[i].className.replace(" selected", "");
-    	selected[i].style.display="none";
-    }
-    
- 	selected = document.querySelectorAll("div.services a.selected");
-    for (i = 0; i < selected.length ; i++) {
-    	selected[i].className = selected[i].className.replace(" selected", "");
-    }
-    
-    // SELECT
-    // Select the category
-    event.currentTarget.className += " selected";
-    
-    // Locate the category block and mark it selected
-    category = document.getElementById(catName + "_services");
-    category.style.display = "block";
-    category.className += " selected";
-    
-    // Now enable the services for the current category only
-   	list = document.getElementsByClassName(catName + " service");
+
+  // About The category to be selected now 
+  // Select the category
+  event.currentTarget.className += " selected";
+  var catName = event.currentTarget.name;
+  
+  // Show the services block for this category
+  category = document.getElementById(catName + "_services");
+  category.style.display = "block";
+  category.className += " selected";
+  
+  // Now reveal the services for the current category only
+ 	list = document.getElementsByClassName(catName + " service");
 	for (i = 0; i < list.length; i++) {
 		service = list[i];
 		service.style.display = "block";
 		
 		// select the first service
 		if (i==0) {
-			openService(event, catName, service.innerText.toLowerCase());
+			openService(event, catIndex, i);
 		}
 	}
 }
 
-function openService(event, catName, serviceName) {
+function openService(event, catIndex, serviceIndex) {
+
+  var catName = data[catIndex].category;
+  var serviceName = data[catIndex].services[serviceIndex].name;
 
 	// First hide the previously displayed service description
  	// Unselect all previously selected 
@@ -61,122 +132,24 @@ function openService(event, catName, serviceName) {
   		selected[i].className += " selected";
   	}
     
-    showServiceDetails(event, catName, serviceName);
+    showServiceDetails(event, catIndex, serviceIndex);
 }
 
- function loaddata() {
- var i, j, category, service, catblock;
- 
- /*
-  			catblock = document.createElement("div");
-  			document.body.appendChild(catblock);
-  			var node = document.createAttribute("class");
-  			node.value = "category";
-  			catblock.setAttributeNode(node);
-  			*/
-  			catblock = document.getElementsByClassName("category")[0];
-  
-    //<a href="javascript:void(0)" name="customer" class="catlink" onclick="openCategory(event, 'customer')">Customer</a>
-    if (catblock == null) {
-    	console.log("Could not find the categories block");
-    }
-    else {
- 	for (i = 0; i < data.length; i++) {
- 		category = data[i];
- 			var a = document.createElement("a");
- 			catblock.appendChild(a);
-  			var node = document.createAttribute("href");
-  			node.value = "javascript:void(0)";
-  			a.setAttributeNode(node);
-  			node = document.createAttribute("name");
-  			node.value = category.category;
-  			a.setAttributeNode(node);
-  			node = document.createAttribute("class");
-  			node.value = "catlink";
-  			a.setAttributeNode(node);
-  			node = document.createAttribute("onclick");
-  			node.value = "openCategory(event, \'" + category.category + "\')";
-  			a.setAttributeNode(node);
-  			a.innerText = category.category.charAt(0).toUpperCase() + category.category.slice(1);
-  			
-  			for (j = 0; j < category.services.length; j++) {
- 				service = category.services[j];
-  			
-   				//  <a href="javascript:void(0)" name="customer_service_create" class="customer service" onclick="openService(event, 'customer', 'create')">Create</a>
-
- 			
- 			}
- 
- 		}
-    }
- }
-/*
-	{
-  		customer : [
-  			{
-  				service : {
- 			name:"Get",
-	 		desc:"Get a Customer", 
-	 		url:"http://medmanage.sytes.net:3000/customer/:contact", 
-	 		method:"GET", 
-	 		data:"\{\"customerContact\"\:9902455333\"\}"
-	 	},
-	 	service : {
- 			name:"Post",
-	 		desc:"Create a Customer", 
-	 		url:"http://medmanage.sytes.net:3000/customer/new", 
-	 		method:"POST", 
-	 		data:"\{\"customerContact\"\:9902455333\"\}"
-	 	}
-	 	]
-	 	
-	 }
-	 ]
-};
-*/
-  			
-
-var services = {
-customercreate: {
-	name:"Create",
-	 desc:"Create a Customer", 
-	 url:"http://medmanage.sytes.net:3000/customer/new", 
-	 method:"POST", 
-	 data:"a bunch of post data. Need to figure out how to escape JSON chanracters"
-	
-},
-customerget : {
-	name:"Get",
-	 desc:"Get a Customer", 
-	 url:"http://medmanage.sytes.net:3000/customer/:contact", 
-	 method:"GET", 
-	 data:"\{\"customerContact\"\:9902455333\"\}"	
-}
-};
-
-function showServiceDetails(event, catName, serviceName) {
+function showServiceDetails(event, catIndex, serviceIndex) {
 	var element, varname, service;
+
+  // Find the category
+  var catName = data[catIndex].category;
+  var serviceName = data[catIndex].services[serviceIndex].name;
 	
-	varname = catName+serviceName;
-	service = services[varname];
+  service = data[catIndex].services[serviceIndex];;
 	
 	if (service != null) {
-		document.getElementById("name").innerText = service.name;
-		document.getElementById("desc").innerText = service.desc;
-		document.getElementById("url").innerText = service.url;
-		document.getElementById("method").innerText = service.method;
-		document.getElementById("data").innerText = service.data;
-	}
-	else {
-		document.getElementById("servicedetail").className.replace(" selected", "");
-		document.getElementById("servicedetail").style.display = "none";
-		
-		document.getElementById("name").innerText = "";
-		document.getElementById("desc").innerText = "";
-		document.getElementById("url").innerText = "";
-		document.getElementById("method").innerText = "";
-		document.getElementById("data").innerText = "";
-		
+		//document.getElementById("name").innerText = (service == null || service.name == null ? "" : service.name);
+		document.getElementById("desc").value = (service == null || service.desc == null ? "" : service.desc);
+		document.getElementById("url").value =  (service == null || service.url == null ? "" : url + service.url);
+		document.getElementById("method").value = (service == null || service.method == null ? "" : service.method);
+		document.getElementById("data").value = (service == null || service.data == null ? "" : service.data);
 	}
 
     // Now reveal the selected service
--- public/services/servicesdata.js
+++ public/services/servicesdata.js
@@ -1,42 +1,78 @@
-var data = 
-  [ 
-  	{ "category": "customer",
-	  "services":
-	    [
-	      { "name": "create",
-	   	    "description": "Create a customer",
-	   	    "url":"http://medmanage.sytes.net:3000/customer/new",
-	 		method:"POST", 
-	 		data:"\{\"customerContact\"\9902455333\"\}"	   	    
+var data = [ 
+    {
+      "category":"customer",
+  	  "services" : 
+  	    [
+  	      { 
+		  	  "name": "create",
+		   	  "description": "Create a customer",
+		 	  "method":"POST", 
+		   	  "url":"/customer/new",
+		 	  "data":"{\"name\":\"Madhu Tadiparthi\", \"contact\": 9448756530, \"email\":\"madhut00@gmail.com\", \"prefVendCont\": 918028450292 }"
+	      },  	    
+	      {
+		      "name": "get",
+		   	  "description": "Get customer information",
+		   	  "url":"/customer/:contactNumber",
+		 	  "method":"GET", 
+		 	  "data":""	   	    
 	      },
-	      { "name": "get",
-	   	    "description": "Get customer information",
-	   	    "url":"http://medmanage.sytes.net:3000/customer/9902455333",
-	 		method:"GET", 
-	 		data:""	   	    
+	      {
+	      	  "name": "update",
+		   	  "description": "Update customer information",
+		   	  "url":"/customer/update/:contactNumber",
+		 	  "method":"POST", 
+		 	  "data":""	      	  
+	      },
+	      {
+	      	  "name": "login",
+		   	  "description": "Login customer",
+		   	  "url":"/customer/login/:contactNumber",
+		 	  "method":"POST", 
+		 	  "data":""	      	  
 	      }
-	    ]
+	    ]   
     },
-    { "category": "order",
-      "services":
+    {
+      "category":"order",
+      "services": 
         [
-	      { "name": "create",
-	   	    "description": "Create an order",
-	   	    "url":"http://medmanage.sytes.net:3000/orders/order/new",
-	 		method:"POST", 
-	 		data:"\{\"customerContact\"\9902455333\"\}"	   	    
-	      }
-	    ]
-	 },
-    { "category": "vendor",
-      "services":
+          {
+		      "name": "create",
+		   	  "description": "Create an order",
+		   	  "url":"/orders/order/new",
+		 	  "method":"POST", 
+		 	  "data":"{\"customerContact\":\"9902455333\"}"	   	    
+	      }	 
+	    ]   
+	},
+    {
+      "category":"vendor",
+      "services": 
         [
-	      { "name": "create",
-	   	    "description": "Create a vendor",
-	   	    "url":"http://medmanage.sytes.net:3000/vendor/new",
-	 		method:"POST", 
-	 		data:"\{\"customerContact\"\9902455333\"\}"	   	    
-	      }
+          {
+	      	  "name": "create",
+		   	  "description": "Create a vendor",
+		   	  "url":"/vendor/new",
+		 	  "method":"POST", 
+		 	  "data":"{\"customerContact\":\"9902455333\"}"	   	    
+	      },
+          {
+	      	  "name": "get",
+		   	  "description": "Retreive a vendor",
+		   	  "url":"/vendor/new",
+		 	  "method":"GET", 
+		 	  "data":""	   	    
+	      },
+          {
+	      	  "name": "update",
+		   	  "description": "Create a vendor",
+		   	  "url":"/vendor/update/:contactNumber",
+		 	  "method":"POST", 
+		 	  "data":"{\"vendorContact\":\"08045936111\"}"	   	    
+	      }	      
 	    ]
-	 }
-  ];
+    }
+];
+
+var url = 'http://medmanage.sytes.net:3000';
